da0d2a27047b41f621b5eb9ebd1f0030eb39d605
coconiemel <cocojn@hotmail.com>
2023-01-03 16:08:27 +0100

    add documentation

--- R/check_function.R
+++ R/check_function.R
@@ -1,3 +1,13 @@
+#' check_function
+#'
+#' @param data that needs to be checked
+#'
+#' @return a list
+#' @export
+#'
+#' @examples
+#' test <- c("A", "B", "C")
+#' check_function(test)
 check_function <- function(x) {
   # Function to check the class type of data "x"
   class_result <- class(x)
--- R/save_function.R
+++ R/save_function.R
@@ -1,3 +1,16 @@
+#' save_function
+#'
+#' @param df dataframe to save as CSV and RSD
+#' @param output_name name of output file and location
+#' @param rownames rownames TRUE or FALSE
+#'
+#' @return CSV and RSD
+#' @export
+#' @importFrom utils write.csv
+#'
+#' @examples
+#' test2 <- data.frame(col1 = c("a", "b", "c"), col2 = c("d", "e", "f"))
+#' save_function(test2, "test2")
 save_function <- function(df, output_name, rownames = FALSE){
   # Function to save data frames into csv and rds files
   write.csv(df, paste0(output_name, ".csv"), row.names = rownames)
