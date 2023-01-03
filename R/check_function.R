09b6c2bb24418528be87fa95f899f5fd52c2a96b
coconiemel <cocojn@hotmail.com>
2023-01-03 15:30:40 +0100

    try

--- R/check_function.R
+++
@@ -1,5 +0,0 @@
-check_class <- function(x) {
-  # Function to check the class type of data "x"
-  class_result <- class(x)
-  print(paste("the class of", x, "is", class_result))
-}
--- R/tutorial.R
+++ R/tutorial.R
@@ -2,7 +2,21 @@ library(devtools)
 
 use_git()
 
-use_r("check_function.R")
+#Function 1
+check_function <- function(x) {
+  # Function to check the class type of data "x"
+  class_result <- class(x)
+  print(paste("the class of", x, "is", class_result))
+}
+
+#Function 2
+save_function <- function(df, output_name, rownames = FALSE){
+  # Function to save data frames into csv and rds files
+  write.csv(df, paste0(output_name, ".csv"), row.names = rownames)
+  saveRDS(df, file=paste0(output_name, ".rds"))
+}
+
+use_r("check_function")
 use_r("save_fucntion.R")
 
 load_all()
