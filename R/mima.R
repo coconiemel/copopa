7df714ee97a62ea463d9f9812f281cc41a988b84
coconiemel <cocojn@hotmail.com>
2023-01-06 12:29:11 +0100

    add mima function to package

---
+++ R/mima.R
@@ -0,0 +1,17 @@
+#' Title
+#'
+#' @param df
+#'
+#' @return a list
+#' @export
+#'
+#' @examples
+#' test3 <- c("1", "2", "3", "4")
+#' mima(test3)
+mima <- function(df){
+  # Function to calculate the minimum and maximum at once
+  mima_list <- list(min = min(df), max = max(df))
+  return(mima_list)
+}
+
+
